66facb224e91b786197cfa95efbe46fc09549c39
홍지학 <5226947@hanmail.net>
2022-04-27 16:03:08 +0900

    문제풀이

--- Project1/5의수난.cpp
+++ Project1/5의수난.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 const int multiNum = 5;
 
-int main()
+int dfasdfsdamain()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
-    string str;
-    cin >> str;
+    string st;
+    cin >> st;
     int answer = 0;
     for (int i = 0; i < multiNum; ++i)
     {
-        int x = str[i] - '0';
+        int x = st[i] - '0';
         answer += pow(x, multiNum);
     }
     cout << answer << '\n';
