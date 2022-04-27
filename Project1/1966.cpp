6355e066fcd882f9e4f4bd0f7a409dbe599a7198
홍지학 <5226947@hanmail.net>
2022-04-27 13:58:26 +0900

    문제풀이

---
+++ Project1/1966.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+vector<int> answer;
+vector<int> sortList;
+queue<int> que;
+int n, m;
+
+void calcul()
+{
+    int idx = -1;
+    for (int i = 0; i < n; ++i)
+    {
+        while (true)
+        {
+            idx = (idx + 1) % n;
+            if (sortList[i] == que.front())
+            {
+                answer[idx] = i + 1;
+                int x = que.front();
+                que.pop();
+                que.push(x);
+                break;
+            }
+            int x = que.front();
+            que.pop();
+            que.push(x);
+        }
+    }
+    while (!que.empty())
+    {
+        que.pop();
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int c;
+    cin >> c;
+    for (int i = 0; i < c; ++i)
+    {
+        cin >> n >> m;
+        sortList.assign(n, 0);
+        answer.assign(n, 0);
+        for (int j = 0; j < n; ++j)
+        {
+            cin >> sortList[j];
+            que.push(sortList[j]);
+        }
+        sort(sortList.begin(), sortList.end(), greater<>());
+        calcul();
+        cout << answer[m] << '\n';
+    }
+
+    return 0;
+}
