0256d414722b261523827b30e54b088801d3607f
홍지학 <5226947@hanmail.net>
2022-04-27 11:43:35 +0900

    문제풀이

---
+++ Project1/2164.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+queue<int> que;
+
+int calcul()
+{
+    while (que.size() != 1)
+    {
+        que.pop();
+        int x = que.front();
+        que.pop();
+        que.push(x);
+    }
+    return que.front();
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+        que.push(i);
+    cout << calcul() << '\n';
+    return 0;
+}
